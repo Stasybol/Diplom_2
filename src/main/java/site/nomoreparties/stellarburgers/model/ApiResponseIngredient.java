26923c402458ad3bf3dc7404e7054e9a6d8be52f
Stasy Bolotnikova <stasybol95@gmail.com>
2025-03-15 12:23:05 +0300

    Model class

---
+++ src/main/java/site/nomoreparties/stellarburgers/model/ApiResponseIngredient.java
@@ -0,0 +1,10 @@
+package site.nomoreparties.stellarburgers.model;
+
+public class ApiResponseIngredient {
+    private IdIngredient[] data;
+
+    public IdIngredient[] getData() {
+        return data;
+    }
+}
+
---
+++ src/main/java/site/nomoreparties/stellarburgers/model/IdIngredient.java
@@ -0,0 +1,12 @@
+package site.nomoreparties.stellarburgers.model;
+
+
+public class IdIngredient {
+    private String _id;
+
+    public String getId() {
+        return _id;
+    }
+
+
+}
